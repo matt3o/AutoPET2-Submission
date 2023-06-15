ba72d0afa79a61d5e922a13ee42bde60e16628bb
Matthias Hadlich <matthiashadlich@posteo.de>
2023-06-15 18:54:03 +0200

    copy_files.sh: Update to new range
    Former-commit-id: c9ca1e42553929139b358d90532f7fe2f1cb7a86

--- copy_files.sh
+++ copy_files.sh
@@ -1,4 +1,4 @@
-for i in {1..45}
+for i in {50..100}
 do
 	scp slurmq:/projects/mhadlich_segmentation/data/"$i"/log.txt data/"$i"_log.txt
 	scp slurmq:/projects/mhadlich_segmentation/data/"$i"/usage.csv data/"$i"_usage.csv

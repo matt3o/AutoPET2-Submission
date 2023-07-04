a13b10531bfaf4cd4414d095e876affc3ff788c3
Matthias Hadlich <matthiashadlich@posteo.de>
2023-07-04 09:47:17 +0200

    Back to dataloader
    Former-commit-id: 6c990dc877f678d6c17d978ae3eafce5edd0b1f3

--- user_guidance_study/train.py
+++ user_guidance_study/train.py
@@ -669,7 +669,7 @@ def main():
 
     # Training
     parser.add_argument("-a", "--amp", default=False, action='store_true')
-    parser.add_argument("--num_workers", type=int, default=1)
+    parser.add_argument("--num_workers", type=int, default=2)
     parser.add_argument("-e", "--epochs", type=int, default=100)
     # If learning rate is set to 0.001, the DiceCE will produce Nans?!?
     parser.add_argument("-lr", "--learning_rate", type=float, default=0.0001)

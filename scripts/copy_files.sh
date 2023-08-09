ae9121d1e85ea45c57e4c0939e358007d43c2a0e
Matthias Hadlich <matthiashadlich@posteo.de>
2023-08-09 13:22:22 +0200

    Remove some data from the repo
    Former-commit-id: a0e79066295330ff420d334a99ee3bfa60af3e79

--- scripts/copy_files.sh
+++
@@ -1,5 +0,0 @@
-for i in {50..100}
-do
-	scp slurmq:/projects/mhadlich_segmentation/data/"$i"/log.txt data/"$i"_log.txt
-	scp slurmq:/projects/mhadlich_segmentation/data/"$i"/usage.csv data/"$i"_usage.csv
-done

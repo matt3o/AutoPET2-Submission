77311b5b50179571fd5c367e7872c70ea4a3af82
Matthias Hadlich <matthiashadlich@posteo.de>
2023-09-06 13:34:24 +0200

    Add Dockerfile
    Former-commit-id: 67b400b0a6a73d4d2bc0e448f4ce57bd7aaf4a7c

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+#FROM python:3.9-slim
+#FROM pytorch/pytorch
+FROM projectmonai/monai:1.2.0
+
+RUN groupadd -r user && useradd -m --no-log-init -r -g user user
+
+RUN mkdir -p /opt/app /input /output \
+    && chown user:user /opt/app /input /output
+
+USER user
+WORKDIR /opt/app
+
+ENV PATH="/home/user/.local/bin:${PATH}"
+
+RUN python -m pip install -U pip
+RUN python -m pip install SimpleITK
+#RUN python -m pip install --user -U pip && python -m pip install --user pip-tools
+
+#COPY --chown=user:user requirements.txt /opt/app/
+#RUN python -m pip install -r requirements.txt
+
+
+#ENV INPUTS_PET="/inputs/images/pet/"
+#ENV INPUTS_CT="/inputs/images/ct/"
+#ENV OUTPUTS="/output/images/automated-petct-lesion-segmentation/"
+#ENV NII_PATH="/opt/app/nifti"
+
+
+COPY --chown=user:user . /opt/app/sw_interactive_segmentation/
+WORKDIR /opt/app/sw_interactive_segmentation/
+ 
+#CMD python src/test.py -i /input/images/pet/ -o /output/images/automated-petct-lesion-segmentation/ --use_scale_intensity_range_percentiled --non_interactive -a --disks --gpu_size small --eval_only --limit_gpu_memory_to 0.66 --resume_from checkpoint.pt
+CMD python src/test.py -i /input/images/pet/ -d /tmp -o /output/images/automated-petct-lesion-segmentation/ --use_scale_intensity_range_percentiled --non_interactive -a --disks --gpu_size small --eval_only --limit_gpu_memory_to 0.66 --resume_from checkpoint_194.pt -ta --dataset AutoPET2_Challenge --dont_check_output_dir --no_log --dont_crop_foreground
+#ENTRYPOINT [ "python", "-m", "process"]
---
+++ test.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+
+set -euf -o pipefail
+
+SCRIPTPATH="$(dirname "$( cd "$(dirname "$0")" ; pwd -P)")"
+SCRIPTPATHCURR="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPTPATH=$SCRIPTPATHCURR
+echo $SCRIPTPATH
+
+./build.sh
+
+VOLUME_SUFFIX=$(dd if=/dev/urandom bs=32 count=1 | md5sum | cut --delimiter=' ' --fields=1)
+MEM_LIMIT="30g"  # Maximum is currently 30g, configurable in your algorithm image settings on grand challenge
+
+VOLUME=unet_baseline-output
+#docker volume create $VOLUME 
+#echo "Volume created, running evaluation"
+#-$VOLUME_SUFFIX
+VOLUME=$SCRIPTPATH/output/
+# Do not change any of the parameters to docker run, these are fixed
+docker run --rm \
+        --memory="${MEM_LIMIT}" \
+        --memory-swap="${MEM_LIMIT}" \
+        --network="none" \
+        --cap-drop="ALL" \
+        --security-opt="no-new-privileges" \
+        --gpus="all"  \
+        --shm-size="128m" \
+        --pids-limit="256" \
+        -v $SCRIPTPATH/test/input/:/input/ \
+        -v $VOLUME:/output/ \
+        sw_segmentation
+
+echo "Evaluation done, checking results"
+docker build -f Dockerfile.eval -t unet_eval .
+
+docker run --rm -it \
+        -v $VOLUME:/output/ \
+        -v $SCRIPTPATH/test/expected_output_uNet/:/expected_output/ \
+        unet_eval python3 -c """
+import SimpleITK as sitk
+import os
+print('Start')
+file = os.listdir('/output/images/automated-petct-lesion-segmentation')[0]
+print(file)
+output = sitk.GetArrayFromImage(sitk.ReadImage(os.path.join('/output/images/automated-petct-lesion-segmentation/', file)))
+expected_output = sitk.GetArrayFromImage(sitk.ReadImage('/expected_output/PRED.nii.gz'))
+mse = sum(sum(sum((output - expected_output) ** 2)))
+if mse <= 10:
+    print('Test passed!')
+else:
+    print(f'Test failed! MSE={mse}')
+"""
+
+#docker volume rm unet_baseline-output-$VOLUME_SUFFIX

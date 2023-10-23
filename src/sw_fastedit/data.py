23b069886165fc69ea07c62c553ba0c5a56f83d9
Matthias Hadlich <matthiashadlich@posteo.de>
2023-10-23 12:45:10 +0200

    Bug: Spacing has been removed from AddGuidanceSignal

--- src/sw_fastedit/data.py
+++ src/sw_fastedit/data.py
@@ -255,7 +255,7 @@ def get_click_transforms(device, args):
             sigma=args.sigma,
             disks=(not args.no_disks),
             device=device,
-            spacing=spacing,
+            # spacing=spacing,
         ),
         ToTensord(keys=("image", "label", "pred"), device=cpu_device)
         if args.sw_cpu_output
